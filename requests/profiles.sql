1c140ecde39ba1124fa2986ff2a299e0f7ace185
Poly Titova <poly_titova@mail.ru>
2021-09-24 22:05:13 +0300

    Дополнили заполнение базы данных

--- requests/profiles.sql
+++ requests/profiles.sql
@@ -1,11 +1,11 @@
-INSERT INTO profiles (`address`, `bd`, `about`, `phone`, `skype`, `role`) VALUES
-('738 Hagan Lane', '1989-03-05', 'Pellentesque ultrices mattis odio.', '75531015353', 'mobile', 'customer'),
-('11 Dovetail Junction', '1989-03-03', 'Suspendisse potenti.', '21468788926', 'Grass-roots', 'executor'),
-('5 Iowa Avenue', '1989-04-18', 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.', '63271348718', 'Team-oriented', 'customer'),
-('6823 Lillian Point', '1989-12-13', 'Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc.', '72882384431', 'intermediate', 'executor'),
-('5303 Village Green Hill', '1989-02-03', 'Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem. Fusce consequat.', '28396220507', 'upward-trending', 'customer'),
-('45 Twin Pines Hill', '1989-07-06', 'Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl. Aenean lectus. Pellentesque eget nunc.', '64890419671', 'background', 'executor'),
-('73 Kedzie Terrace', '1989-11-07', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.', '27052074771', 'coherent', 'customer'),
-('67 Northwestern Center', '1989-07-07', 'Aliquam erat volutpat. In congue.', '75569924500', 'Programmable', 'executor'),
-('507 Graceland Junction', '1989-03-19', 'Suspendisse potenti.', '12403580562', 'knowledge base', 'customer'),
-('8 Ridgeview Trail', '1989-12-21', 'Cras pellentesque volutpat dui.', '76657531985', 'solution', 'executor');
+INSERT INTO profiles (`address`, `bd`, `phone`, `skype`, `role`) VALUES
+('738 Hagan Lane', '1989-03-05', '75531015353', 'mobile'),
+('11 Dovetail Junction', '1989-03-03', '21468788926', 'Grass-roots'),
+('5 Iowa Avenue', '1989-04-18', '63271348718', 'Team-oriented'),
+('6823 Lillian Point', '1989-12-13', '72882384431', 'intermediate'),
+('5303 Village Green Hill', '1989-02-03', '28396220507', 'upward-trending'),
+('45 Twin Pines Hill', '1989-07-06', '64890419671', 'background'),
+('73 Kedzie Terrace', '1989-11-07', '27052074771', 'coherent'),
+('67 Northwestern Center', '1989-07-07', '75569924500', 'Programmable'),
+('507 Graceland Junction', '1989-03-19', '12403580562', 'knowledge base'),
+('8 Ridgeview Trail', '1989-12-21', '76657531985', 'solution');
---
+++ requests/reviews.sql
@@ -0,0 +1,6 @@
+INSERT INTO reviews (`executor_id`, `customer_id`, `description`, `rate`) VALUES
+('21', '22', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '3.5'),
+('23', '24', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.', '2.3'),
+('25', '26', 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.', '1.6'),
+('27', '28', 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.', '4.1'),
+('29', '30', 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?', '2.8');
